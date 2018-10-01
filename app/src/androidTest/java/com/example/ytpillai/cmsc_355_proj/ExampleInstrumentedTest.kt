0c05cde7ed45ac874c2badc30288d0c25b8db52f
Yadunandan Pillai <Yadunandan Pillai>
2018-10-01 13:12:56 -0400

    Initial shell commit

---
+++ app/src/androidTest/java/com/example/ytpillai/cmsc_355_proj/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.ytpillai.cmsc_355_proj
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.ytpillai.cmsc_355_proj", appContext.packageName)
+    }
+}
