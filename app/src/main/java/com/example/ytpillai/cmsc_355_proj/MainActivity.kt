4572bd139ba321fe56e282bab711cc44d4095b37
Council Laptop <jeremylumanbailey@gmail.com>
2018-10-10 11:46:07 -0400

    Added security lock to application.
    Refer to ProgramActivity if you want to start adding on other activities
    NOT MainActivity as MainActivity is basically just a loading screen for the app right now.
    
    Still need to add confirmation when user is creating pattern lock.
    
    Still need to add Espresso or unit tests.
    
    Added noinspection GradleCompatible to fix error (build.gradle).
    
    Probably next iteration:
    
    Add ability to change lock without reinstall entire application.
    
    Add fingerprint security.
    
    Add regular PIN or password instead of just a pattern lock.

--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/MainActivity.kt
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.ytpillai.cmsc_355_proj
 
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +11,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*This activity is basically just a startup screen that goes to security lock.
+        Go to ProgramActivity.kt for the applications actual content */
+
+        val handler = Handler()
+        handler.postDelayed({
+            val preferences = getSharedPreferences("PREFS", 0)
+            val password = preferences.getString("password", "0")
+            if (password == "0") {
+                val intent = Intent(applicationContext, CreatePasswordActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(applicationContext, InputPasswordActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 2000)
     }
 }
