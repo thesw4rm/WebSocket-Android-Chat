2da3e7fee489b1760e04b3c3900466274c6de5c1
Yadunandan Pillai <Yadunandan Pillai>
2018-10-12 19:23:53 -0400

    Hi daniel would you like some fries with that

---
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/Contacts.kt
@@ -0,0 +1,12 @@
+package com.example.ytpillai.cmsc_355_proj
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Contacts : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contacts)
+    }
+}
