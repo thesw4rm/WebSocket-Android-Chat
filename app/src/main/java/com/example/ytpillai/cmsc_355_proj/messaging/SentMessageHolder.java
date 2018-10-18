8aad0ff7fb21020cf75e89272fc56b82df9687cd
Danny Mc <dannymc21@users.noreply.github.com>
2018-10-18 05:44:11 -0400

    temp

---
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/messaging/SentMessageHolder.java
@@ -0,0 +1,32 @@
+package com.example.ytpillai.cmsc_355_proj.messaging;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ytpillai.cmsc_355_proj.R;
+
+private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
+    TextView messageText, timeText, nameText;
+    ImageView profileImage;
+
+    ReceivedMessageHolder(View itemView) {
+        super(itemView);
+        messageText = itemView.findViewById(R.id.text_message_body);
+        timeText = itemView.findViewById(R.id.text_message_time);
+        nameText = itemView.findViewById(R.id.text_message_name);
+        //profileImage = itemView.findViewById(R.id.image_message_profile);
+    }
+
+    void bind(UserMessage message) {
+        messageText.setText(message.getMessage());
+
+        // Format the stored timestamp into a readable String using method.
+        timeText.setText(Utils.formatDateTime(message.getCreatedAt()));
+        nameText.setText(message.getSender().getNickname());
+
+        // Insert the profile image from the URL into the ImageView.
+        Utils.displayRoundImageFromUrl(mContext, message.getSender().getProfileUrl(), profileImage);
+    }
+}
