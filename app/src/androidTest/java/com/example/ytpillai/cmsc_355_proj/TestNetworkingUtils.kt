796f63243b889e75921f3ffc274d5fdb42247865
Yadunandan Pillai <ytpillai@gmail.com>
2018-10-18 01:51:40 -0400

    Finished working network stuff, finished working websocketserver. Need to figure out how to check if websocketserver is already running (right now just gonna check the port to see if it is used.

--- app/src/androidTest/java/com/example/ytpillai/cmsc_355_proj/TestNetworkingUtils.kt
+++ app/src/androidTest/java/com/example/ytpillai/cmsc_355_proj/TestNetworkingUtils.kt
@@ -1,19 +1,34 @@
 package com.example.ytpillai.cmsc_355_proj
 
+import android.content.Intent
+import android.os.IBinder
+import android.support.test.InstrumentationRegistry
+import android.support.test.rule.ServiceTestRule
 import android.support.test.runner.AndroidJUnit4
-import android.test.suitebuilder.annotation.MediumTest
-import com.example.ytpillai.cmsc_355_proj.networking.NetworkingUtils
 import org.junit.Assert.assertNotNull
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.TimeoutException
 
 @RunWith(AndroidJUnit4::class)
+
 class TestNetworkingUtils {
+    @get:Rule
+    val mMessageServiceRule = ServiceTestRule()
 
     @Test
+    @Throws(TimeoutException::class)
     fun testgetIpAddress(){
-        val ipAddress = NetworkingUtils.getWifiIPAddress()
-        println(ipAddress.toString())
-        assertNotNull(ipAddress)
+        val serviceIntent = Intent(
+                InstrumentationRegistry.getTargetContext(),
+                MessageService::class.java
+        )
+
+        val binder: IBinder = mMessageServiceRule.bindService(serviceIntent)
+        val service: MessageService = (binder as MessageService.LocalBinder).service
+        assertNotNull(service.getIpAddressIHope())
+
+
     }
 }
--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/messaging/MessageSocketServer.kt
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/messaging/MessageSocketServer.kt
@@ -1,44 +1,56 @@
 package com.example.ytpillai.cmsc_355_proj.messaging
 
+import android.content.Context
+import android.content.Intent
 import android.util.Log
+import com.example.ytpillai.cmsc_355_proj.ConversationActivity
+import com.example.ytpillai.cmsc_355_proj.ProgramActivity
+import com.example.ytpillai.cmsc_355_proj.R
 import org.java_websocket.WebSocket
 import org.java_websocket.handshake.ClientHandshake
 import org.java_websocket.server.WebSocketServer
 import java.lang.Exception
 import java.net.InetSocketAddress
 
-class MessageSocketServer(address: InetSocketAddress) : WebSocketServer(address) {
+class MessageSocketServer(address: InetSocketAddress, var context: Context) : WebSocketServer(address) {
 
     /**
      * Message socket server open handler. Eventually will do handshake stuff
      */
     override fun onOpen(conn: WebSocket?, handshake: ClientHandshake?) {
-        if(conn == null){
+        if (conn == null) {
             Log.e("MESSAGE_SOCKET_SERVER", "Tried to open a client's connection the ws server on this device, but got null connection. ")
         }
         conn!!.send("Ya I got the connection bro!") //Need to make this do important stuff like initial handshake stuff and things
+
     }
 
     override fun onClose(conn: WebSocket?, code: Int, reason: String?, remote: Boolean) {
-        if(conn == null){
+        if (conn == null) {
             Log.e("MESSAGE_SOCKET_SERVER", "Tried to close websocket server but got null connection. ")
         }
         conn!!.close(200, "Closing the connection because why not?")
     }
 
     override fun onMessage(conn: WebSocket?, message: String?) {
-        if(conn == null){
+        if (conn == null)
             Log.e("MESSAGE_SOCKET_SERVER", "Tried to receive message on websocket server but got null connection. ")
-        }
-        Log.d("MESSAGE_SOCKET_SERVER", "I GOT UR MESSAGE $message")
+        else
+            Log.d("MESSAGE_SOCKET_SERVER", "I GOT UR MESSAGE $message")
+
+        val intent = Intent(context, ProgramActivity::class.java)
+        intent.action = context.resources.getString(R.string.ACTION_START)
+        intent.putExtra("message", message)
+        context.sendBroadcast(intent)
+
     }
 
     override fun onStart() {
-        Log.d("MESSAGE_SOCKET_SERVER", "WebSocketgpg - server started.")
+        Log.d("MESSAGE_SOCKET_SERVER", "WebSocket - server started.")
     }
 
     override fun onError(conn: WebSocket?, ex: Exception?) {
-        Log.e("MESSAGE_SOCKET_SERVER", "Got an error: " + ex.toString())
+        Log.e("MESSAGE_SOCKET_SERVER", "Got an error in socket", ex)
     }
 
 }
