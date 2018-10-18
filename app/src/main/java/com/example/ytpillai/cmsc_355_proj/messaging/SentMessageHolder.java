c93b8d7bf984e394a16ebb63bc92fcd1b7e0016e
Yadunandan Pillai <ytpillai@gmail.com>
2018-10-18 06:17:59 -0400

    no

--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/messaging/SentMessageHolder.java
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/messaging/SentMessageHolder.java
@@ -8,25 +8,25 @@ import android.widget.TextView;
 import com.example.ytpillai.cmsc_355_proj.R;
 
 private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
-    TextView messageText, timeText, nameText;
+    private TextView messageText, timeText, nameText;
     ImageView profileImage;
 
     ReceivedMessageHolder(View itemView) {
         super(itemView);
-        messageText = itemView.findViewById(R.id.text_message_body);
-        timeText = itemView.findViewById(R.id.text_message_time);
-        nameText = itemView.findViewById(R.id.text_message_name);
+        messageText = itemView.findViewById(R.id.ChatBody);
+      //  timeText = itemView.findViewById(R.id.text_message_time);
+      //  nameText = itemView.findViewById(R.id.text_message_name);
         //profileImage = itemView.findViewById(R.id.image_message_profile);
     }
 
-    void bind(UserMessage message) {
+    void bind(Message message) {
         messageText.setText(message.getMessage());
 
         // Format the stored timestamp into a readable String using method.
-        timeText.setText(Utils.formatDateTime(message.getCreatedAt()));
-        nameText.setText(message.getSender().getNickname());
+    //    timeText.setText(Utils.formatDateTime(message.getCreatedAt()));
+    //    nameText.setText(message.getSender().getNickname());
 
         // Insert the profile image from the URL into the ImageView.
-        Utils.displayRoundImageFromUrl(mContext, message.getSender().getProfileUrl(), profileImage);
+     //   Utils.displayRoundImageFromUrl(mContext, message.getSender().getProfileUrl(), profileImage);
     }
 }
