a51ed751b23f4e97b9a38cf826ea51da46957267
Daniel McNamara <43069859+dannymc21@users.noreply.github.com>
2018-11-07 14:25:24 -0500

    created an app icon, added Rubik font, and more UI fixes

--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/ContactsActivity.kt
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/ContactsActivity.kt
@@ -3,6 +3,7 @@ package com.example.ytpillai.cmsc_355_proj
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_contacts.*
 
 class ContactsActivity : AppCompatActivity() {
@@ -12,8 +13,11 @@ class ContactsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_contacts)
 
         newConBtn.setOnClickListener{
-            val intent = Intent(this,ProgramActivity::class.java)
+            //Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ProgramActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/ConversationActivity.kt
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/ConversationActivity.kt
@@ -1,14 +1,24 @@
 package com.example.ytpillai.cmsc_355_proj
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.ytpillai.cmsc_355_proj.messaging.App
+import com.example.ytpillai.cmsc_355_proj.messaging.ChatService
 import com.example.ytpillai.cmsc_355_proj.messaging.Message
 import com.example.ytpillai.cmsc_355_proj.messaging.MessageAdapter
 import kotlinx.android.synthetic.main.activity_conversation.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.util.*
+
+private const val TAG = "ConversationActivity"
 
 class ConversationActivity : AppCompatActivity() {
 
@@ -22,6 +32,7 @@ class ConversationActivity : AppCompatActivity() {
         backArrowBtn.setOnClickListener {
             val intent = Intent(this,ContactsActivity::class.java)
             startActivity(intent)
+            finish()
         }
 
         chatRecycler.layoutManager = LinearLayoutManager(this)
@@ -31,17 +42,59 @@ class ConversationActivity : AppCompatActivity() {
 
 
         sendBtn.setOnClickListener{
-            if(chatbox.text != null) {
+            if(chatbox.text.isNotEmpty()) {
+                val message = Message(
+                        App.nickname,
+                        chatbox.text.toString(),
+                        Calendar.getInstance().timeInMillis
+                        )
 
-                val message = Message(App.nickname, chatbox.text.toString(), Calendar.getInstance().timeInMillis)
-                adapter.addMessage(message)
-                chatRecycler.scrollToPosition(adapter.itemCount - 1)
+                val call = ChatService.create().postMessage(message)
+
+                call.enqueue(object : Callback<Void> {
+                    override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                        resetInput()
+                        if (!response.isSuccessful) {
+                            Log.e(TAG, response.code().toString());
+                            Toast.makeText(applicationContext,"Response was not successful", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<Void>, t: Throwable) {
+                        resetInput()
+                        Log.e(TAG, t.toString())
+                        Toast.makeText(applicationContext,"Error when calling the service", Toast.LENGTH_SHORT).show()
+                    }
+                })
             }
+
+
         }
 
-
-
+        //sendMessage()
     }
 
+    private fun resetInput() {
+        // Clean text box
+        chatbox.text.clear()
+
+        // Hide keyboard
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(currentFocus!!.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+    }
+
+    /*
+    private fun sendMessage(){
+
+        val message = Message("nickname", "message", "time".toLong())
+        Log.e(TAG, message.toString())
+
+        runOnUiThread {
+            adapter.addMessage(message)
+            chatRecycler.scrollToPosition(adapter.itemCount - 1)
+        }
+
+    }
+    */
 
 }
