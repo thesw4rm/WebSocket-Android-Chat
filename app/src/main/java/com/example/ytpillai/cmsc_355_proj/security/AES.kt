9bda1400a6d3ed9f602534139b6bfcd9d8967295
John Naylor <jonaylor89@gmail.com>
2018-11-09 13:29:51 -0500

    Document methods

--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/security/AES.kt
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/security/AES.kt
@@ -2,10 +2,22 @@ package com.example.ytpillai.cmsc_355_proj.security
 
 class AES : Cipher {
 
+    /**
+     * Encrypt plain text with key associated with the alias
+     *
+     * @plainText: Message string
+     * @alias: Name associated with the key
+     */
     override fun encrypt(plainText: String, alias: String): String? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    /**
+     * Decrypt cipher text with key associated with the alias
+     *
+     * @cipherText: Encoded message
+     * @alias: Name associated with the key
+     */
     override fun decrypt(cipherText: String, alias: String): String? {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
