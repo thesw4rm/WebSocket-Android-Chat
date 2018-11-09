62afc49c23fa00db68c410a95344e83748a51f5b
Council Laptop <jeremylumanbailey@gmail.com>
2018-11-09 11:01:51 -0500

    Vibrate

--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/InputPasswordActivity.kt
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/InputPasswordActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ytpillai.cmsc_355_proj
 
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Vibrator
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
@@ -20,6 +22,8 @@ class InputPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_password)
+
+        val vibratorService = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
 
         val preferences = getSharedPreferences("PREFS", 0)
         password = preferences.getString("password", "0")
@@ -43,6 +47,7 @@ class InputPasswordActivity : AppCompatActivity() {
                     startActivity(intent)
                     finish()
                 } else {
+                    vibratorService.vibrate(100)
                     Toast.makeText(this@InputPasswordActivity, "Wrong password", Toast.LENGTH_SHORT).show()
                     mPatternLockView.clearPattern()
                 }
