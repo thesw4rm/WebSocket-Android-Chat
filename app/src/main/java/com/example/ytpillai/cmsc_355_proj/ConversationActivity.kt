fa938884c2687b38443f35dad3c4f5843cc3eb2c
Yadunandan Pillai <ytpillai@gmail.com>
2018-11-16 00:10:41 -0500

    Did a lot of things

--- app/src/main/java/com/example/ytpillai/cmsc_355_proj/ConversationActivity.kt
+++ app/src/main/java/com/example/ytpillai/cmsc_355_proj/ConversationActivity.kt
@@ -4,17 +4,28 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import com.example.ytpillai.cmsc_355_proj.messaging.*
+import com.example.ytpillai.cmsc_355_proj.messaging.App
+import com.example.ytpillai.cmsc_355_proj.messaging.Me
+import com.example.ytpillai.cmsc_355_proj.messaging.Message
+import com.example.ytpillai.cmsc_355_proj.messaging.MessageAdapter
+import com.example.ytpillai.cmsc_355_proj.networking.MessageSocketClient
+import com.example.ytpillai.cmsc_355_proj.services.MessageClientService
 import com.example.ytpillai.cmsc_355_proj.services.MessageServerService
+import com.tinder.scarlet.Scarlet
 import kotlinx.android.synthetic.main.activity_conversation.*
+import java.net.URI
 import java.util.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 
 private const val TAG = "ConversationActivity"
 
@@ -22,7 +33,7 @@ class ConversationActivity : AppCompatActivity() {
 
 
     private lateinit var adapter: MessageAdapter
-
+    private lateinit var messageClientService: MessageClientService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,8 +66,29 @@ class ConversationActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter text to send", Toast.LENGTH_SHORT).show()
             }
         }
-        val messageServiceIntent = Intent(this, MessageServerService::class.java)
-        this.startService(messageServiceIntent)
+
+
+        /*val messageServiceIntent = Intent(this, MessageServerService::class.java)
+        this.startService(messageServiceIntent)*/
+
+
+
+        messageClientService = MessageClientService()
+        val messageClientServiceIntent = Intent(this, MessageClientService::class.java)
+        messageClientServiceIntent.putExtra("destIP", "10.0.2.2")
+        startService(messageClientServiceIntent)
+        val destIP = "10.0.2.2"
+        val uri = "ws://$destIP:8112/"
+        //TODO: Destroy this somehow in onDestroy
+        val request = Request.Builder().url(uri).build()
+        val okHttpClient = OkHttpClient.Builder().build()
+
+        val messageSocketClient = MessageSocketClient(URI(uri), applicationContext)
+        val webSocket = okHttpClient.newWebSocket(request, messageSocketClient)
+
+        val okHttpClientService = okHttpClient.dispatcher().executorService()
+
+
         val messageBroadcastReceiver = object : BroadcastReceiver() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 if (intent!!.action!!.equals(resources.getString(R.string.ACTION_RECEIVED_MESSAGE))) {
@@ -66,7 +98,9 @@ class ConversationActivity : AppCompatActivity() {
                             Log.d("PROGRAM_ACTIVITY", "Message received : ${intent.extras!!["message"]}")
 
                         }
+
                     }
+                    receiveMessage("${intent.extras!!["message"]}")
                 } else {
                     Log.d("PROGRAM_ACTIVITY", "Something received : ${intent.extras!!["message"]}")
                 }
@@ -77,13 +111,14 @@ class ConversationActivity : AppCompatActivity() {
 
     }
 
+
     private fun sendMessage() {
 
-        Me.ip = "0"
+        Me.ip = "127.0.0.1"
         val message = Message(
 //                App.nickname,
                 Me.ip,
-                chatbox.text.toString(),
+                "test",
                 Calendar.getInstance().timeInMillis
         )
 
@@ -96,10 +131,10 @@ class ConversationActivity : AppCompatActivity() {
 
     }
 
-    private fun receiveMessage() {
+    private fun receiveMessage(yourmom: String) {
         val message = Message(
                 App.ip,
-                "testing",
+                yourmom,
                 Calendar.getInstance().timeInMillis
         )
 
@@ -108,7 +143,6 @@ class ConversationActivity : AppCompatActivity() {
             chatRecycler.scrollToPosition(adapter.itemCount - 1)
         }
 
-        TODO("Somehow be able to receive messages")
 
     }
 
