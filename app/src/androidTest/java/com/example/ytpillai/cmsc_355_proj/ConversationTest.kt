11404236477d29d7b7d9c1a66933f14dca2cd74f
Daniel McNamara <43069859+dannymc21@users.noreply.github.com>
2018-12-04 19:19:34 -0500

    Conversation Test & Contacts Test 2.0

--- app/src/androidTest/java/com/example/ytpillai/cmsc_355_proj/ConversationTest.kt
+++ app/src/androidTest/java/com/example/ytpillai/cmsc_355_proj/ConversationTest.kt
@@ -4,31 +4,38 @@ package com.example.ytpillai.cmsc_355_proj
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.action.ViewActions.*
 import android.support.test.espresso.assertion.ViewAssertions.matches
+import android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.View
 import android.view.ViewGroup
 import org.hamcrest.Description
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.allOf
 import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class ContactsActivityTest2 {
+class ConversationTest {
 
     @Rule
     @JvmField
     var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
-    fun contactsActivityTest2() {
+    fun conversationTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        Thread.sleep(600)
+
         val appCompatButton = onView(
                 allOf(withId(R.id.SkipPass), withText("Set up Later"),
                         childAtPosition(
@@ -39,78 +46,74 @@ class ContactsActivityTest2 {
                         isDisplayed()))
         appCompatButton.perform(click())
 
-
-        val appCompatEditText = onView(
+        val textInputEditText = onView(
                 allOf(withId(R.id.nickname),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(android.R.id.content),
+                                        withId(R.id.nicknameHolder),
                                         0),
-                                1),
+                                0),
                         isDisplayed()))
-        appCompatEditText.perform(click())
+        textInputEditText.perform(replaceText("Danny Mc"), closeSoftKeyboard())
 
-        val appCompatEditText2 = onView(
-                allOf(withId(R.id.nickname),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(android.R.id.content),
-                                        0),
-                                1),
-                        isDisplayed()))
-        appCompatEditText2.perform(replaceText("Danny Mc"), closeSoftKeyboard())
-
-        val appCompatEditText3 = onView(
+        val textInputEditText2 = onView(
                 allOf(withId(R.id.ipAddress),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(android.R.id.content),
+                                        withId(R.id.ipAddressHolder),
                                         0),
-                                2),
+                                0),
                         isDisplayed()))
-        appCompatEditText3.perform(replaceText("192.168.1.1"), closeSoftKeyboard())
+        textInputEditText2.perform(replaceText("128.172.245.82"), closeSoftKeyboard())
 
-        val appCompatButton3 = onView(
+        val appCompatButton2 = onView(
                 allOf(withId(R.id.startChat), withText("Start Chat"),
                         childAtPosition(
                                 childAtPosition(
                                         withId(android.R.id.content),
                                         0),
-                                3),
+                                1),
+                        isDisplayed()))
+        appCompatButton2.perform(click())
+
+        val appCompatEditText = onView(
+                allOf(withId(R.id.chatbox),
+                        childAtPosition(
+                                allOf(withId(R.id.chatboxLayout),
+                                        childAtPosition(
+                                                withClassName(`is`("android.support.constraint.ConstraintLayout")),
+                                                3)),
+                                0),
+                        isDisplayed()))
+        appCompatEditText.perform(replaceText("Hello world!"), closeSoftKeyboard())
+
+        val appCompatButton3 = onView(
+                allOf(withId(R.id.sendBtn),
+                        childAtPosition(
+                                allOf(withId(R.id.chatboxLayout),
+                                        childAtPosition(
+                                                withClassName(`is`("android.support.constraint.ConstraintLayout")),
+                                                3)),
+                                1),
                         isDisplayed()))
         appCompatButton3.perform(click())
 
-        val appCompatImageButton = onView(
-                allOf(withId(R.id.backArrowBtn),
+        val textView = onView(
+                allOf(withId(R.id.ChatBodySent), withText("Hello world!"),
                         childAtPosition(
                                 childAtPosition(
-                                        withId(android.R.id.content),
+                                        withId(R.id.chatRecycler),
                                         0),
                                 0),
                         isDisplayed()))
-        appCompatImageButton.perform(click())
+        textView.check(matches(withText("Hello world!")))
 
-        val viewGroup = onView(
-                allOf(withId(R.id.contactRecyclerView),
+        val recyclerView = onView(
+                allOf(withId(R.id.chatRecycler),
                         childAtPosition(
-                                allOf(withId(R.id.contactList),
-                                        childAtPosition(
-                                                IsInstanceOf.instanceOf(android.view.ViewGroup::class.java),
-                                                2)),
-                                0),
-                        isDisplayed()))
-        viewGroup.check(matches(isDisplayed()))
-
-        val textView = onView(
-                allOf(withId(R.id.nicknameContactList), withText("Danny Mc"),
-                        childAtPosition(
-                                allOf(withId(R.id.contactRecyclerView),
-                                        childAtPosition(
-                                                withId(R.id.contactList),
-                                                0)),
-                                2),
-                        isDisplayed()))
-        textView.check(matches(withText("Danny Mc")))
+                                withClassName(`is`("android.support.constraint.ConstraintLayout")),
+                                2)))
+        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(0, click()))
     }
 
     private fun childAtPosition(
